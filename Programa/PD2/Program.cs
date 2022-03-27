094ba36e30c0a9e60120c6a69727f12748d60db6
Augustas Leliūnas <69753515+Gustuxas4862@users.noreply.github.com>
2022-03-27 22:27:27 +0300

    Add files via upload

---
+++ Programa/PD2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program was created by Augustas Leliūnas. Student group id PI20B.
+
+using PD2;
+using System;
+using System.Windows.Forms;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainPanel());
+    }
+}
